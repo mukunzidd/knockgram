8f773f1ddaf0d752129afcb40b7d810002c1b212
Dodo <dmukunzi0@gmail.com>
2020-10-02 07:51:59 +0200

    Initial Commit

---
+++ src/components/Title.js
@@ -0,0 +1,14 @@
+/* eslint-disable jsx-a11y/accessible-emoji */
+import React from 'react';
+
+const Title = () => {
+  return (
+    <div className="title">
+      <h1>FireGram</h1>
+      <h2>Your Pictures</h2>
+      <p>I cheap Instagram knock off, made with ReactJS and 🔥base .</p>
+    </div>
+  );
+};
+
+export default Title;
